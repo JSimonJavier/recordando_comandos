c293177d6c848dc752a6f5514d535a4bf15c34ae
Javier Simon <simonjavier.fullstack@gmail.com>
2023-08-30 10:23:27 -0300

    chore: agregar contenido

---
+++ contenido.js
@@ -0,0 +1,22 @@
+/*
+  DEPLOY -> vamos a compartir nuestro proyecto al mundo -> RENDER
+  objetivos:
+    - hacer el deploy
+    - variables de entorno -> libreria dotenv
+    - armar un readme
+    - refactorizar nuestro codigo para evitar repetir logica
+
+? DEPLOY
+  - proceso de llevar una app o pagina de su estado de desarrollo a un ambiente donde puede ser utilizado por usuario finales
+  - tarea compleja y desafiante
+  - importa => porque programar entrega su trabajo final
+  - ayuda al armado del PORTFOLIO -> coleccion de trabajos o proyectos de una persona
+  - una vez ya deployado -> CUALQUIER PERSONA PUEDE VER EL PROYECTO
+
+  *github -> sitio donde solemos consultar los trabajo realizados de un desarrollor
+    - recordotario de comandos:
+      git add
+      git status 
+      git commit -m "mensaje"
+      git push
+*/
